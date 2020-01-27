8f3d13daf2a8de504e0a39f4ed1af6d3f6369bda
MoochMcGee <mszoopers@protonmail.com>
2020-01-27 14:36:31 -0600

    Moar cross-platform and shiet

--- src/main.zig
+++ src/main.zig
@@ -22,6 +22,24 @@ const cpu_808x_flags = packed struct {
     direction: u1 = 0,
     overflow: u1 = 0,
     reserved4: u4 = 0xf,
+
+    pub fn set_flags(self: @This(), value: u16) void {
+        carry = value & 1;
+        parity = (value >> 2) & 1;
+        adjust = (value >> 4) & 1;
+        zero = (value >> 6) & 1;
+        sign = (value >> 7) & 1;
+        trap = (value >> 8) & 1;
+        interrupt = (value >> 9) & 1;
+        direction = (value >> 10) & 1;
+        overflow = (value >> 11) & 1;
+    }
+
+    pub fn get_flags(self: @This()) u16 {
+        return carry | (@as(u16, parity) << 2) | (@as(u16, adjust) << 4) | (@as(u16, zero) << 6) |
+        (@as(u16, sign) << 7) | (@as(u16, trap) << 8) | (@as(u16, interrupt) << 9) | (@as(u16, direction) << 10) |
+        (@as(u16, overflow) << 11);
+    }
 };
 
 pub const cpu_808x = struct {
@@ -72,7 +90,7 @@ pub fn main() anyerror!void {
     const machine_name = args[1];
     if(mem.eql(u8, machine_name, "ibmxt"))
     {
-        const rom_path = "roms\\machines\\ibmpc\\BIOS_5150_24APR81_U33.BIN";
+        const rom_path = try std.fs.path.join(allocator, &([_][]const u8{"roms", "machines", "ibmpc", "BIOS_5150_24APR81_U33.BIN"}));
         var machine = try pcmachine.new(allocator, rom_path);
 
         std.debug.warn("Opcode: {0x:0<2}\n", .{machine.rom[((machine.cpu.segs[1] << 4) + machine.cpu.ip) & 0x1fff]});
